0397bdb47af84f41333deb68a482cfaf27ccf0bb
Thiyagarajan <159250555+Thiya2001@users.noreply.github.com>
2025-11-09 21:49:37 +0530

    Add files via upload

---
+++ Operators/Relational Operator/Relational.java
@@ -0,0 +1,16 @@
+public class Relational {
+    public static void main(String[] args) {
+        boolean res = 4 == 5;
+        System.out.println(res);
+        boolean res1 = 5 > 6;
+        System.out.println(res1);
+        boolean res2 = 12 < 32;
+        System.out.println(res2);
+        boolean res3 = 43 >= 33;
+        System.out.println(res3);
+        boolean res4 = 62 <= 32;
+        System.out.println(res4);
+        boolean res5 = 7 != 7;
+        System.out.println(res5);
+    }
+}
